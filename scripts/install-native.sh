41b01fe47f46df78382e9b97db289262c6d03cf7
Paris Kolios <paris.k@gmail.com>
2017-09-04 15:30:35 +0200

    Add psequel to install-native.sh

--- scripts/install-native.sh
+++ scripts/install-native.sh
@@ -3,6 +3,7 @@
 brew cask install firefoxnightly
 brew cask install google-chrome
 brew cask install sequel-pro
+brew cask install psequel
 brew cask install slack
 brew cask install the-unarchiver
 brew cask install virtualbox
