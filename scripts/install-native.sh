10a88d6c4b567b917084c2a7af5b0cfcc4b1633b
Paris Kolios <paris.k@gmail.com>
2017-09-25 00:29:26 +0200

    Use iterm2 nigthly & fix typography

--- scripts/install-native.sh
+++ scripts/install-native.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-brew cask install iterm2
+brew cask install iterm2-nightly
 brew cask install firefoxnightly
 brew cask install google-chrome
 brew cask install sequel-pro
-brew cask install psequel
 brew cask install slack
 brew cask install the-unarchiver
 brew cask install virtualbox
