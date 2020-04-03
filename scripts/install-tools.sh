1c347437059100d2980c4acfc9686b45a55504c2
Paris Kolios <paris.k@gmail.com>
2020-04-03 17:42:17 +0200

    Cleanup brew install tools

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -3,8 +3,11 @@
 # Install GNU core utilities (those that come with macOS are outdated).
 brew install coreutils
 
-# Git
 brew install git
+brew install neovim
+brew install ripgrep
+brew install tree
+brew install wget
 
 # Python
 brew instal python
@@ -16,21 +19,8 @@ pip3 install flake8-bugbear
 pip3 install black
 pip3 install isort
 
-# Docker
-# brew install docker
-# pip install docker-compose
-# brew install docker-machine-nfs
-
 # AWS
-# pip install awscli
-# pip install awsebcli
+pip install awscli
+pip install awsebcli
 
-# The rest
-brew install ripgrep
-brew install tree
-# brew install vim --with-python3
-brew install wget
-brew install go
-
-# Remove outdated versions from the cellar.
 brew cleanup
