0dd63e732364f37731f05df8b41d9552f1a6515b
Paris Kolios <paris.k@gmail.com>
2020-04-03 17:19:13 +0200

    Update brew install command

--- scripts/install-brew.sh
+++ scripts/install-brew.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Make sure we’re using the latest Homebrew
 brew update
