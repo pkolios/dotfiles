23a8e8e03799e10a730c13ac51ea7468e799786a
Paris Kolios <paris.k@gmail.com>
2020-04-03 19:48:15 +0200

    Fix nvim config install

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -sv ~/.dotfiles/init.vim nvim/init.vim
+mkdir -p ~/.config/nvim
+ln -sv ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 python3 -m pip install --user --upgrade pynvim
 python3 -m pip install --user --upgrade jedi
 python3 -m pip install --user --upgrade 'python-language-server[all]'
