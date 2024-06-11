afe4ce58f8e44d3dbc5a4c04f3c3ba0c67ecb2c7
Paris Kolios <paris.k@gmail.com>
2024-06-11 18:06:20 +0300

    Fix firefox touchpad scrolling

--- users/enc/home/desktop/firefox.nix
+++ users/enc/home/desktop/firefox.nix
@@ -11,10 +11,19 @@
       ];
 
       settings = {
+        "apz.gtk.kinetic_scroll.enabled" = "true";
+        "apz.overscroll.enabled" = "true";
+        "apz.gtk.pangesture.enabled" = "true";
         "browser.aboutConfig.showWarning" = false;
         "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
         "browser.newtabpage.activity-stream.topSitesRows" = 2;
         "browser.tabs.warnOnClose" = true;
+	"general.smoothScroll.mouseWheel.durationMaxMS" = 500;
+	"general.smoothScroll.mouseWheel.durationMinMS" = 250;
+	"mousewheel.acceleration.start" = 1;
+	"mousewheel.default.delta_multiplier_x" = 20;
+	"mousewheel.default.delta_multiplier_y" = 20;
+	"mousewheel.default.delta_multiplier_z" = 20;
         "privacy.trackingprotection.enabled" = true;
         "toolkit.telemetry.pioneer-new-studies-available" = false;
         "toolkit.telemetry.reportingpolicy.firstRun" = false;
