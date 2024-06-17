f92008d961af5681861c58d4c772abf3dfbe6d80
Paris Kolios <paris.k@gmail.com>
2024-06-17 18:27:51 +0300

    Add xclip alias to copy to clipboard

--- users/enc/home/cli/default.nix
+++ users/enc/home/cli/default.nix
@@ -1,19 +1,30 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./docker-compose.nix
-    ./git.nix
-    ./neovim
-    ./ripgrep.nix
-  ];
+  imports = [ ./docker-compose.nix ./git.nix ./neovim ./ripgrep.nix ];
 
-  home.packages = with pkgs; [
-    p7zip
-    tree
-    xclip
-  ];
+  home.packages = with pkgs; [ p7zip tree xclip ];
 
   programs.btop.enable = true;
   programs.eza.enable = true;
+
+  home.shellAliases = {
+    # ls, mv, rm, cp
+    ls = "eza";
+    l = "eza -lAh";
+    la = "eza -lah";
+    ll = "eza -lh";
+    mv = "mv -v";
+    rm = "rm -i -v";
+    cp = "cp -v";
+
+    # xclip copy to clipboard
+    xclip = "xclip -selection clipboard";
+
+    # nix
+    nix-rebuild = "sudo nixos-rebuild switch --flake .#nyx";
+    nix-clean =
+      "sudo nix profile wipe-history --profile /nix/var/nix/profiles/system  --older-than 7d";
+    nix-gc = "sudo nix-collect-garbage --delete-old";
+  };
 }
--- users/enc/home/shell/default.nix
+++ users/enc/home/shell/default.nix
@@ -1,32 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./alacritty.nix
-    ./fish.nix
-    ./starship.nix
-    ./tmux.nix
-  ];
+  imports = [ ./alacritty.nix ./fish.nix ./starship.nix ./tmux.nix ];
 
   home.sessionVariables = {
     EDITOR = "nvim";
     BROWSER = "firefox";
     TERMINAL = "alacritty";
-  };
-
-  home.shellAliases = {
-    # ls, mv, rm, cp
-    ls = "eza";
-    l = "eza -lAh";
-    la = "eza -lah";
-    ll = "eza -lh";
-    mv = "mv -v";
-    rm = "rm -i -v";
-    cp = "cp -v";
-
-    # nix
-    nix-rebuild = "sudo nixos-rebuild switch --flake .#nyx";
-    nix-clean = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/system  --older-than 7d";
-    nix-gc = "sudo nix-collect-garbage --delete-old";
   };
 }
