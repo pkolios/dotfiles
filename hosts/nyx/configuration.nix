f0efae17830917a7b165dd733d5aa168318436e2
Paris Kolios <paris.k@gmail.com>
2024-06-21 11:18:26 +0300

    Disable rootless docker setting

--- hosts/nyx/configuration.nix
+++ hosts/nyx/configuration.nix
@@ -79,8 +79,8 @@
   virtualisation.docker = {
     enable = true;
     rootless = {
-      enable = true;
-      setSocketVariable = true;
+      enable = false;
+      # setSocketVariable = true;
     };
   };
 
