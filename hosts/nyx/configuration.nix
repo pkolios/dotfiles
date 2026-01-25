10098c3a6aa5a8776e38d69866ff81e1e857d4ef
Paris Kolios <paris.k@gmail.com>
2026-01-25 19:10:06 +0200

    chore(networking): Allow firework local dev tcp ports

--- hosts/nyx/configuration.nix
+++ hosts/nyx/configuration.nix
@@ -172,7 +172,11 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [
+    5173
+    8000
+    8080
+  ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
