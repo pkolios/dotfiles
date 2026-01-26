204399b9bba452fa71177c4f812bcce1bca4b214
Paris Kolios <paris.k@gmail.com>
2026-01-26 18:14:11 +0200

    fix(nvim): Use correct treesitter config

--- users/common/cli/neovim/config/lua/plugins.lua
+++ users/common/cli/neovim/config/lua/plugins.lua
@@ -460,26 +460,17 @@ local cmp = require("cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 -- Highlight, edit, and navigate code
-local configs = require("nvim-treesitter.configs")
-configs.setup({
-	highlight = {
-		enable = true,
-		disable = function(_, buf)
-			local max_filesize = 100 * 1024 -- 100 KiB
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-				return true
-			end
-		end,
-		-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-		--  If you are experiencing weird indenting issues, add the language to
-		--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-		additional_vim_regex_highlighting = { "ruby" },
-	},
-	indent = {
-		enable = true,
-		disable = { "ruby" },
-	},
+-- nvim-treesitter 1.0+ uses Neovim's built-in treesitter integration
+-- Parsers are installed via Nix (nvim-treesitter.withAllGrammars)
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		local max_filesize = 10 * 1024 * 1024 -- 10 MiB
+		local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(0))
+		if ok and stats and stats.size > max_filesize then
+			return
+		end
+		pcall(vim.treesitter.start)
+	end,
 })
 -- There are additional nvim-treesitter modules that you can use to interact
 -- with nvim-treesitter. You should go explore a few and see what interests you:
